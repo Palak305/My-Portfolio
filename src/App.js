856d3de8976a6a39945ae46c1f90bf370e290af8
palakperiwal <palak.periwal@amoga.io>
2023-12-25 17:51:44 +0530

    portfolio | 1

--- src/App.js
+++ src/App.js
@@ -26,15 +26,15 @@ const APP = () => {
       <Grid
         container
         alignItems="center"
-        justifyContent="flex-end"
+        justifyContent="space-around"
         spacing={2}
         px={1}
-        sx={{ height: "60px", background: "red" }}
+        sx={{ height: "60px", background: "#A8A196" }}
       >
         <Grid item>
           <a className={classes.anchorLink} href="#about">
             {" "}
-            about me
+            About Me
           </a>
         </Grid>
         <Grid item>
@@ -43,14 +43,78 @@ const APP = () => {
             Education
           </a>
         </Grid>
+        <Grid item>
+          <a className={classes.anchorLink} href="#skills">
+            {" "}
+            Skills
+          </a>
+        </Grid>  
+        <Grid item>
+          <a className={classes.anchorLink} href="#experirence">
+            {" "}
+            Experience
+          </a>
+        </Grid>  
+        <Grid item>
+          <a className={classes.anchorLink} href="#projects">
+            {" "}
+            Projects
+          </a>
+        </Grid>  
+        <Grid item>
+          <a className={classes.anchorLink} href="#contact">
+            {" "}
+            Contact Me
+          </a>
+        </Grid>
       </Grid>
       <Grid container alignItems={"center"} justifyContent={"center"} sx={{ height: "calc(100vh - 40px)" }}>
-        <p>
-          people Call me <b>Miss PP</b>
-        </p>
+        <Grid component={motion.div} variants={variantImg} initial="hidden" animate="visible" item xs={12} sm={4}>
+          <Typography variant="h4" color="white">
+            Hi, I am Palak Periwal.
+          </Typography>
+          <Typography variant="h6" color="white">
+            A Frontend ReactJS Developer👩‍💻
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <motion.img
+            variants={variantImg}
+            initial="hidden"
+            animate="visible"
+            src="https://media.licdn.com/dms/image/D4D03AQE4AFoz78Z2GQ/profile-displayphoto-shrink_800_800/0/1694086768301?e=1709164800&v=beta&t=p7ulrVXr5YbKJbDl2ATPPdSIWWbJHeJ3AVMhK860ugI"
+            alt="profile"
+            className={classes.profile}
+          />
+        </Grid>
       </Grid>
-      <Grid id="education" container alignItems={"center"} justifyContent={"center"} sx={{}}>
-        I am education
+      <Grid id="about" container alignItems={"center"} justifyContent={"space-between"}>
+      <Grid item>
+          <Typography variant="h4" color="white">
+            About Me
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Slide direction="up" in={true} mountOnEnter unmountOnExit>
+            <Box
+              border={1}
+              borderRadius={8}
+              borderColor="#91C8E4"
+              p={2}
+              style={{ width: "100%", boxSizing: "border-box" }}
+            >
+              <Typography>
+                👋 Hi there! I'm Palak, a passionate and creative Front-End Developer with a keen eye for design and a
+                love for crafting seamless user experiences. 🌐 My expertise lies in HTML, CSS, and JavaScript, and I'm
+                well-versed in modern front-end frameworks like React. 🚀 In addition to technical skills, I'm a strong
+                communicator and a collaborative team player. I believe in the power of teamwork and open communication
+                to deliver exceptional results. 🌟 If you're looking for a front-end developer who is not only skilled
+                but also passionate about their work, I'd love to connect and explore how I can contribute to your
+                team's success. Let's build something awesome together!
+              </Typography>
+            </Box>
+          </Slide>
+        </Grid>
       </Grid>
     </div>
   );
